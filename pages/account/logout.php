404c17a504bf3335ed6d9f7916fe1d7e36aa931f
phhajek <hajek.ph@gmail.com>
2020-12-23 02:46:54 +0100

    Worked on 2fa kinda buggy

--- pages/account/logout.php
+++ pages/account/logout.php
@@ -9,6 +9,7 @@ if (!empty($_POST['currentPage'])) {
 		unset($_SESSION['dc_access_token']);
 		unset($_SESSION['user_username']);
 		unset($_SESSION['user_id']);
+		unset($_SESSION['control_2FA']);
 		session_destroy();
 		session_start();
 		$_SESSION['logoutsuccess'] = '<span class="text-warning">No activity within 1440 seconds; please log in again.</span>';
@@ -19,6 +20,7 @@ if (!empty($_POST['currentPage'])) {
 		unset($_SESSION['dc_access_token']);
 		unset($_SESSION['user_username']);
 		unset($_SESSION['user_id']);
+		unset($_SESSION['control_2FA']);
 		session_destroy();
 		session_start();
 		$_SESSION['logoutsuccess'] = '<span class="text-danger">Your account has been successfully deleted.</span>';
@@ -29,6 +31,7 @@ if (!empty($_POST['currentPage'])) {
 		unset($_SESSION['dc_access_token']);
 		unset($_SESSION['user_username']);
 		unset($_SESSION['user_id']);
+		unset($_SESSION['control_2FA']);
 		session_destroy();
 		session_start();
 		$_SESSION['logoutsuccess'] = '<span class="text-danger">Your account has been banned by '.$_SESSION['user_blocked_by'].' for the reason: '.$_SESSION['user_blocked_reason'].'.
@@ -40,6 +43,7 @@ if (!empty($_POST['currentPage'])) {
 		unset($_SESSION['dc_access_token']);
 		unset($_SESSION['user_username']);
 		unset($_SESSION['user_id']);
+		unset($_SESSION['control_2FA']);
 		session_destroy();
 		session_start();
 		$_SESSION['logoutsuccess'] = '<span class="text-danger">An error occured. You got logged out.</span>';
@@ -50,6 +54,7 @@ if (!empty($_POST['currentPage'])) {
 		unset($_SESSION['dc_access_token']);
 		unset($_SESSION['user_username']);
 		unset($_SESSION['user_id']);
+		unset($_SESSION['control_2FA']);
 		session_destroy();
 		session_start();
 		$_SESSION['logoutsuccess'] = '<span class="text-success">You have been successfully logged out.</span>';
--- pages/account/two-factor-authentication.php
+++ pages/account/two-factor-authentication.php
@@ -1,111 +1,51 @@
 <?php
-include('./include/header-banner.php');
+
+if (empty($_SESSION['user_id'])) {
+   header('location: /logout');
+}
 
 session_start();
-
-if (!isset($_SESSION['user_id'])) {
-	header('location: /account/logout/');
-	exit();
-}
-
-if($_GET['rq'] == 1) {
-
-   if ($_SESSION['user_oauth_provider'] == "Google LLC." && empty($_COOKIE[$cookie_name]) || $_SESSION['user_oauth_provider'] == "Google LLC" && empty($_COOKIE[$cookie_name])) {
-
-      
-     function generateRandomString($length = 24) {
-      return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
-   }
-
-   $token = generateRandomString();
+require "Authenticator.php";
 
 
-      $userid = $_POST['value'];
-      $code = $token;
-      
-      if(!isset($_SESSION['2facode'])) {
-         $_SESSION['2facode'] = $code;
-      }
-      
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL,"http://85.214.166.192:8081");
-      curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, "action=enable2FA&userid=$userid&token=$code");
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      $response = curl_exec($ch);
-      curl_close($ch);
-
-      if(startsWith($response, "Success")) {
-         $typeAlert = "success";
-      } else if(startsWith($response, "Error")) {
-         $typeAlert = "danger";
-      } else {
-         $typeAlert = "primary";
-      }
-
-      $_SESSION['dc_id_temp'] = $_POST['value'];
-
-      $pl = "Insert the 2FA token";
-      $btn = "Confirm two factor authentification";
-      $pattern = "\b[A-Za-z0-9]{24}\b";
-      $type = "password";
-      $action = "/pages/account/helper/2fa.enable.php";
-
-      $_SESSION['success'] = '<div class="alert alert-'.$typeAlert.'" alert-dismissible fade show center" role="alert">
-                     '.$response.'
-                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                     </button>
-                  </div>';
-   } elseif ($_SESSION['user_oauth_provider'] == "Discord Inc." && empty($_COOKIE[$cookie_name]) || $_SESSION['user_oauth_provider'] == "Discord Inc" && empty($_COOKIE[$cookie_name])) {
-      // Email @phhajek
-   } 
-} else {
-   if ($_SESSION['user_oauth_provider'] == "Google LLC." || $_SESSION['user_oauth_provider'] == "Google LLC") {
-      $pl = "Please enter your Discord ID, e.g. 469208494260617217";
-      $btn = "Request token";
-      $pattern = "\d{17,21}";
-      $type = "text";
-      $action = "?rq=1";
-   } elseif ($_SESSION['user_oauth_provider'] == "Discord Inc." || $_SESSION['user_oauth_provider'] == "Discord Inc") {
-      $pl = "Please enter your email address";
-      $btn = "Request mail";
-      $pattern = "\b[A-Z0-9a-z]+@[A-Z0-9.-]+\.[A-Z]{2,}\b";
-      $type = "text";
-      $action = "?rq=1";
-   } 
+$Authenticator = new Authenticator();
+if (!isset($_SESSION['auth_secret'])) {
+   $secret = $Authenticator->generateRandomSecret();
+   $_SESSION['auth_secret'] = $secret;
 }
 
 
-function startsWith( $haystack, $needle ) {
-   $length = strlen( $needle );
-   return substr( $haystack, 0, $length ) === $needle;
+$qrCodeUrl = $Authenticator->getQR('FiveMods', $_SESSION['auth_secret']);
+
+
+if (!isset($_SESSION['failed'])) {
+   $_SESSION['failed'] = false;
 }
+
 ?>
-<section class="pt-5 pb-5">
-   <div class="container">
-      <div class="row d-flex justify-content-center">
-      <div class="col-md-7">
-         <?php echo $_SESSION['success']; 
-         unset($_SESSION['success']);
-         ?>
-         <div class="card border border-success">
-            <div class="card-body text-center p-5">
-            <h3 class="pb-2 h3 mt-1">Two-Factor Authentication</h3>
-            <p class="lead">Ensure the security of your account - enable the Two-Factor Authentication. </p>
-            <form action="<?php echo $action; ?>" method="post">
-               <input type="text" name="call" value="callFunc" hidden>
-					<input type="text" name="id" value="<?php echo $_SESSION['user_id']; ?>" hidden>
-					<input type="text" name="mail" value="<?php echo $_SESSION['user_email']; ?>" hidden>
-               <input type="text" name="dcid" value="<?php echo $_SESSION['dc_id_temp']; ?>" hidden>
-               <input class="form-control mt-md-3" type="<?php echo $type;?>" pattern="<?php echo $pattern; ?>" name="value" placeholder="<?php echo $pl; ?>" required>
-               <button type="submit" class="btn btn-xs btn-round btn-sm btn-success btn-rised mt-md-3"><?php echo $btn; ?></button> <br>
-               <a href="/account/">Back</a>
-            </form>
+<div class="container">
+   <div class="row">
+      <div class="col-md-6 offset-md-3 mb-5" style="background: white; padding: 20px; box-shadow: 10px 10px 5px #888888; margin-top: 125px;">
+         <h1>Two-Factor Authentication</h1>
+         <hr>
+         <form action="/pages/account/check.php" method="post">
+            <div style="text-align: center;">
+               <?php if ($_SESSION['failed']) : ?>
+                  <div class="alert alert-danger" role="alert">
+                     <strong>Oh snap!</strong> Invalid Code.
+                  </div>
+                  <?php
+                  $_SESSION['failed'] = false;
+                  ?>
+               <?php endif ?>
+
+               <img style="text-align: center;;" class="img-fluid" src="<?php echo $qrCodeUrl ?>" alt="Verify this Google Authenticator"><br><br>
+               <input type="text" class="form-control" name="code" placeholder="******" style="font-size: xx-large;width:200px;border-radius: 0px;text-align: center;display: inline;color: #0275d8;"><br> <br>
+               <button type="submit" class="btn btn-md btn-primary" style="width: 200px;border-radius: 0px;">Verify</button>
+
             </div>
-         </div>
-      </div>
+
+         </form>
       </div>
    </div>
-</section>
+</div>
