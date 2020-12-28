c6dd3ec8b2d7e816265a998eb876c667a765764f
AwesomeCore1 <51826058+AwesomeCore1@users.noreply.github.com>
2020-12-28 09:36:05 +0100

    Added database check

--- pages/ref.php
+++ pages/ref.php
@@ -1,5 +1,11 @@
 <?php 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('max_execution_time', 300);
+
+require_once('./config.php');
+
 if (isset($_GET['rdc'])) {
 
     $ref = urldecode($_GET['rdc']);
@@ -25,8 +31,15 @@ if (isset($_GET['rdc'])) {
     "joinmy.site",
     "crabrave.pw"
    ); 
-   
-    if (in_array($ref, $marks)) { 
+
+    $sql = "SELECT marked_links FROM links WHERE marked_links LIKE '$ref'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $link = $row['marked_links'];
+        }
 
         $check = 1;
 
@@ -37,7 +50,7 @@ if (isset($_GET['rdc'])) {
             <div class="card border border-danger">
                 <div class="card-body text-center p-5">
                 <h2 class="fas fa-exclamation-triangle text-danger"></h2>
-                <h3 class="pb-2 h3 mt-1 text-danger">'.$ref.'</h3>
+                <h3 class="pb-2 h3 mt-1 text-danger">'.$link.'</h3>
                 <p class="lead">This site is external and we <b>can\'t guarantee the safety</b>, always make sure to be on a website <b>using a SSL certificate</b>.</p>
                 <h5>This link grabs personal information, <b>we won\'t redirect you</b>.</h5> 
                 </div>
@@ -53,6 +66,18 @@ if (isset($_GET['rdc'])) {
 
     } else { 
 
+        if (strpos($ref, 'https://') !==false) {
+            $link = $ref;
+        } else {
+            if (strpos($ref, 'http://') !==false) {
+                $strip = str_replace('http://', '', $ref);
+                $link = 'https://'.$strip;
+            } else {
+                $link = 'https://'.$ref;
+            }
+        }
+
+
         $check = 0;
 
         $output1 = '<section class="pt-5 pb-5">
@@ -61,10 +86,10 @@ if (isset($_GET['rdc'])) {
         <div class="col-md-7">
             <div class="card border border-success">
                 <div class="card-body text-center p-5">
-                <h3 class="pb-2 h3 mt-1 '.$css_text.'">'.$ref.'</h3>
+                <h3 class="pb-2 h3 mt-1 '.$css_text.'">'.$link.'</h3>
                 <p class="lead">This site is external and we <b>can\'t guarantee the safety</b>, always make sure to be on a website <b>using a SSL certificate</b>.</p>
-                <a href="'.$ref.'" class="btn btn-xs btn-round btn-lg btn-success btn-rised mt-md-3">Continue and leave page</a> <br>
-                <a href="'.$ref.'" class="btn btn-xs btn-round btn-sm btn-light btn-rised mt-md-3" target="_blank">Open link in new tab</a>
+                <a href="'.$link.'" class="btn btn-xs btn-round btn-lg btn-success btn-rised mt-md-3">Continue and leave page</a> <br>
+                <a href="'.$link.'" class="btn btn-xs btn-round btn-sm btn-light btn-rised mt-md-3" target="_blank">Open link in new tab</a>
                 </div>
             </div>
         </div>
