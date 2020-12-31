3b3103acfbc5845e327677bd19302d12d92164b6
phhajek <hajek.ph@gmail.com>
2020-12-31 18:07:04 +0100

    Disabled errors

--- pages/account/sign-in.php
+++ pages/account/sign-in.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 ini_set('max_execution_time', 300);
 
 include('./include/header-banner.php');
