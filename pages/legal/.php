11a043c256162163626a952a8ddadb5c18b4983c
phhajek <hajek.ph@gmail.com>
2021-01-20 22:32:39 +0100

    Adjusted image size on legal start page

--- pages/legal/.php
+++ pages/legal/.php
@@ -1,7 +1,7 @@
 <?php include('./include/header-banner.php'); ?>
 <section class="pt-5 pb-5">
     <div class="container">
-        <h2 class="text-center font-weight-light mb-5">FiveMods Legal Center</h2>
+        <h2 class="text-center font-weight-light mb-5 pb-5">FiveMods Legal Center</h2>
     </div>
     <div class="cards  text-dark">
         <div class="cards-overlay">
@@ -11,7 +11,7 @@
                         <a href="/legal/privacy-policy/">
                             <div class="card h-100 border-0">
                                 <div class="card-img-top">
-                                    <img src="/static-assets/img/svg/legal/privacy.svg" class="img-fluid mx-auto d-block" alt="<?php echo $lang['privacy-policy']; ?>-IMG">
+                                    <img src="/static-assets/img/svg/legal/privacy.svg" class="img-fluid mx-auto d-block" width="100px" alt="<?php echo $lang['privacy-policy']; ?>-IMG">
                                 </div>
                                 <div class="card-body text-center">
                                     <h4 class="card-title">
@@ -25,7 +25,7 @@
                         <a href="/legal/legal-notice/">
                             <div class="card h-100 border-0">
                                 <div class="card-img-top">
-                                    <img src="/static-assets/img/svg/legal/terms.svg" class="img-fluid mx-auto d-block" alt="<?php echo $lang['legal-notice']; ?>-IMG">
+                                    <img src="/static-assets/img/svg/legal/terms.svg" class="img-fluid mx-auto d-block" width="100px" alt="<?php echo $lang['legal-notice']; ?>-IMG">
                                 </div>
                                 <div class="card-body text-center">
                                     <h4 class="card-title">
@@ -39,7 +39,7 @@
                         <a href="/legal/terms-of-service/">
                             <div class="card h-100 border-0">
                                 <div class="card-img-top mt-2 pt-1">
-                                    <img src="/static-assets/img/svg/legal/settings.svg" class="img-fluid mx-auto d-block" alt="<?php echo $lang['terms-of-service']; ?>-IMG">
+                                    <img src="/static-assets/img/svg/legal/settings.svg" class="img-fluid mx-auto d-block" width="100px" alt="<?php echo $lang['terms-of-service']; ?>-IMG">
                                 </div>
                                 <div class="card-body text-center">
                                     <h4 class="card-title">
@@ -53,7 +53,7 @@
                         <a href="/legal/cookie-consent/">
                             <div class="card h-100 border-0">
                                 <div class="card-img-top mt-3 pt-1">
-                                    <img src="/static-assets/img/svg/legal/cookie.svg" class="img-fluid mx-auto d-block" alt="<?php echo $lang['cookie-consent']; ?>-IMG">
+                                    <img src="/static-assets/img/svg/legal/cookie.svg" class="img-fluid mx-auto d-block" width="100px" alt="<?php echo $lang['cookie-consent']; ?>-IMG">
                                 </div>
                                 <div class="card-body text-center">
                                     <h4 class="card-title">
@@ -69,8 +69,8 @@
                     <div class=" col-sm-6 col-md-3 mb-4">
                         <a href="/legal/upload-policy/">
                             <div class="card h-100 border-0">
-                                <div class="card-img-top mt-3 pt-1">
-                                    <img src="/static-assets/img/svg/legal/upload.svg" class="img-fluid mx-auto d-block" alt="<?php echo $lang['upload-policy']; ?>-IMG">
+                                <div class="card-img-top mt-2 pt-1">
+                                    <img src="/static-assets/img/svg/legal/upload.svg" class="img-fluid mx-auto d-block" width="100px" alt="<?php echo $lang['upload-policy']; ?>-IMG">
                                 </div>
                                 <div class="card-body text-center">
                                     <h4 class="card-title">
@@ -84,7 +84,7 @@
                         <a href="/legal/account-policy/">
                             <div class="card h-100 border-0">
                                 <div class="card-img-top mt-2">
-                                    <img src="/static-assets/img/svg/legal/account.svg" class="img-fluid mx-auto d-block" alt="Account-Policy-IMG">
+                                    <img src="/static-assets/img/svg/legal/account.svg" class="img-fluid mx-auto d-block" width="100px" alt="Account-Policy-IMG">
                                 </div>
                                 <div class="card-body text-center">
                                     <h4 class="card-title">
@@ -97,8 +97,8 @@
                     <div class=" col-sm-6 col-md-3 mb-4">
                         <a href="/legal/payment-agreement/">
                             <div class="card h-100 border-0">
-                                <div class="card-img-top mt-5 pt-1">
-                                    <img src="/static-assets/img/svg/legal/payment.svg" class="img-fluid mx-auto d-block" alt="Payment-Agreement-IMG">
+                                <div class="card-img-top mt-4 pt-1">
+                                    <img src="/static-assets/img/svg/legal/payment.svg" class="img-fluid mx-auto d-block" width="100px" alt="Payment-Agreement-IMG">
                                 </div>
                                 <div class="card-body text-center">
                                     <h4 class="card-title">
