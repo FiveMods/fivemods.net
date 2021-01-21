fff7b58ec487ae5fccd5ea5654c45742949e5b5b
phhajek <hajek.ph@gmail.com>
2021-01-21 18:50:37 +0100

    90/100 legal rework

---
+++ pages/legal/imprint.php
@@ -0,0 +1,97 @@
+<script src="/static-assets/js/sticky-block.js"></script>
+<section class="bg-dark mt-5 pt-5 pb-4 text-white">
+    <div class="container pt-md-4 pb-md-4 height-md-50">
+        <div class="row">
+            <div class="col-md-8 col-lg-7 col-xl-6">
+                <h1 class="display-4"> Impressum </h1>
+                <p class="lead mb-0"> Last change: Thursday 01/21/2020 </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Section -->
+
+<!-- Terms Content Section -->
+<div class="container mt-5">
+    <div class="row">
+        <div id="stickyBlockStartPoint" class="col-md-4 col-lg-3 mb-7 mb-md-0">
+            <!-- Nav -->
+            <nav class="js-sticky-block card card-bordered">
+                <div class="card-body">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <h4 class="text-left"><u>Table of content</u></h4>
+                            <ol class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link mb-1">A. Kontakt</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-1">B. Représentant</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-1">C. Streitschlichtung</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-1">D. Haftung für Inhalte</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-1">E. Haftung für Links</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-1">F. Urheberrecht</a>
+                                </li>
+                            </ol>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- End Nav -->
+        </div>
+
+        <div class="col-md-8 col-lg-9">
+            <article class="pb-2">
+                <h3>Kontakt</h3>
+                <p>
+                    Mailboxde.com GmbH<br>
+                    Zoios, ID 134607<br>
+                    Äussere Weberstr. 57<br>
+                    02763 Zittau, GERMANY
+                </p>
+                <p>Web: www.fivemods.net</p>
+                <p>E-Mail: fivemods.management@gmail.com</p>
+                <h3>Représentant</h3>
+                <p>
+                    Tim Spengler<br>
+                    Philipp Hajek<br>
+                </p>
+
+                <h3>Streitschlichtung</h3>
+                <p>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: https://ec.europa.eu/consumers/odr.
+                    Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+                <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+                <h3>Haftung für Inhalte</h3>
+                <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+
+                <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+                <h3>Haftung für Links</h3>
+                <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p>
+
+                <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+                <h3>Urheberrecht</h3>
+                <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
+
+                <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+
+                </p>
+                <p class="small mt-5 text-center">
+                    <b class="text-dark">by FiveMods.net</b>
+                </p>
+            </article>
+        </div>
+    </div>
+</div>
+<!-- End Terms Content Section -->
