7a3163c829b845929e66baaa784d1f9da6c40755
phhajek <hajek.ph@gmail.com>
2021-01-26 18:52:35 +0100

    Anti VPN/Proxy embed

--- index.php
+++ index.php
@@ -33,6 +33,41 @@ if (empty($_GET['fm_design']) == "orange") {
    $css_text = 'text text-gray';
 }
 
+function shapeSpace_block_proxy_visits()
+{
+
+   $ports = array(80, 81, 553, 554, 1080, 3128, 4480, 6588, 8000, 8080);
+
+   foreach ($ports as $port) {
+      if (@fsockopen($_SERVER['REMOTE_ADDR'], $port, $errno, $errstr, 5)) {
+         header('location: /error/proxy/');
+         die();
+      }
+   }
+}
+
+function get_ip_address($proxy = false)
+{
+   if ($proxy === true) {
+      foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED') as $key) {
+         if (array_key_exists($key, $_SERVER) === true) {
+            foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip) {
+               if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+                  return $ip;
+               }
+            }
+         }
+      }
+   }
+
+   return $_SERVER['REMOTE_ADDR'];
+}
+
+if (get_ip_address() !== get_ip_address(true)) {
+   header('location: /error/proxy/');
+   die();
+}
+
 require_once('./config.php');
 
 
@@ -562,52 +597,6 @@ require_once('./config.php');
       // add padding top to show content behind navbar
       $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
    </script>
-   <!--
-   <script>
-      const charactersList = document.getElementById('charactersList');
-      const searchBar = document.getElementById('searchBar');
-      let hpCharacters = [];
-      
-      searchBar.addEventListener('keyup', (e) => {
-         const searchString = e.target.value.toLowerCase();
-
-         const filteredCharacters = hpCharacters.filter((character) => {
-            return (
-               character.name.toLowerCase().includes(searchString) ||
-               character.house.toLowerCase().includes(searchString)
-            );
-         });
-         displayCharacters(filteredCharacters);
-      });
-      
-      const loadCharacters = async () => {
-         try {
-            const res = await fetch('https://hp-api.herokuapp.com/api/characters');
-            hpCharacters = await res.json();
-            displayCharacters(hpCharacters);
-         } catch (err) {
-            console.error(err);
-         }
-      };
-
-      const displayCharacters = (characters) => {
-         const htmlString = characters
-            .map((character) => {
-               return `
-                     <li class="character">
-                        <h2>${character.name}</h2>
-                        <p>House: ${character.house}</p>
-                        <img src="${character.image}"></img>
-                     </li>
-               `;
-            })
-            .join('');
-         charactersList.innerHTML = htmlString;
-      };
-
-      loadCharacters();
-   </script>
--->
 </body>
 
 </html>
