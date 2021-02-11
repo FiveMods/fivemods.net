a342827579f90ffc0f906910150b74c2fa0f4092
phhajek <hajek.ph@gmail.com>
2021-02-11 10:03:56 +0100

    Fixed more issues

--- include/footer-normal.php
+++ include/footer-normal.php
@@ -5,36 +5,39 @@
             <div class="col-12 col-lg-2 text-lg-left">
             </div>
             <div class="col-12 col-lg-8 mt-4 mt-lg-0">
-               <img alt="image" src="./static-assets/img/brand-down.png" height="40">
+               <img alt="image" src="/static-assets/img/brand-down.png" height="40">
                <p class="text-h5 text-white mt-4"><?php echo $lang['copyright']; ?></p>
             </div>
             <div class="col-12 col-lg-2 text-lg-right mt-4 mt-lg-0">
             </div>
          </div>
       </div>
-      <hr>
+      <hr style="background-color:lightgray;margin-left:30%;margin-right:30%;">
       <div class="container">
          <div class="row text-center align-items-center">
             <div class="col">
                <ul class="nav justify-content-center">
                   <li class="nav-item">
-                     <a class="nav-link active" href="#">Home</a>
+                     <a class="nav-link active" href="/"><?php echo $lang['home']; ?></a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="#">Features</a>
+                     <a class="nav-link" href="/search/"><?php echo $lang['search']; ?></a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="#">Privacy Policy</a>
+                     <a class="nav-link" href="/help-center/"><?php echo $lang['help-center']; ?></a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="#">Terms</a>
+                     <a class="nav-link" href="/legal/"><?php echo $lang['legal-notice']; ?></a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="#">About</a>
+                     <a class="nav-link" href="/language/"><?php echo $lang['language']; ?></a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="/account/sign-in/"><?php echo $lang['log-in']; ?></a>
                   </li>
                </ul>
-               <span style="font-size:8px;">
-               <?php echo $lang['subfooter']; ?>
+               <span style="font-size:8px;color:#676475;">
+               <?php echo $lang['footer-subtext']; ?>
                FiveM® is © 2016-2021 by Cfx.re. </span>
             </div>
          </div>
