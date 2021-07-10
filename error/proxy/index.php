ee0f9ba684381187e44a77d5b33dbd8e8f939660
phhajek <hajek.ph@gmail.com>
2021-07-10 18:00:48 +0200

    Fredney revert revert

--- error/proxy/index.php
+++ error/proxy/index.php
@@ -1,56 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="en" dir="ltr">
 <head>
-    <meta charset="UTF-8">
+    <link rel="stylesheet" id="pagestyle" href="https://assets.fivemods.net/static-assets/css/style.css">
+    <link rel="stylesheet" href="/static-assets/css/error-document.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="icon" href="https://assets.fivemods.net/static-assets/img/svg/brand/svg/fivemods_brand_icon_watermark_primary_1500x1500.svg">
+    <link rel="apple-touch-icon" href="https://img-cdn.fivemods.net/unsafe/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="https://img-cdn.fivemods.net/unsafe/57x57/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="https://img-cdn.fivemods.net/unsafe/72x72/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="https://img-cdn.fivemods.net/unsafe/76x76/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="https://img-cdn.fivemods.net/unsafe/114x114/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="https://img-cdn.fivemods.net/unsafe/120x120/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="https://img-cdn.fivemods.net/unsafe/144x144/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="https://img-cdn.fivemods.net/unsafe/152x152/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://img-cdn.fivemods.net/unsafe/180x180/filters:format(webp):quality(95)/https://assets.fivemods.net/static-assets/img/apple-touch-icon/apple-touch-icon-180x180.png" />
+    <title>Proxy or VPN service detected - FiveMods.net</title>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FiveMods.net - Proxy or VPN service detected.</title>
-    <meta http-equiv="content-language" content="en" />
-    <meta http-equiv="X-Frame-Options" content="SAMEORIGIN">
-    <link rel="icon" href="https://cdn.discordapp.com/attachments/662762008504041522/737797664221167666/fivemods.png">
-    <style>
-        * {
-            background-color: #17141f;
-        }
-
-        .center {
-            text-align: center;
-            margin-top: 35vh;
-            font-family: Arial, Helvetica, sans-serif;
-            color: #ff8637;
-            font-size: 12px;
-        }
-
-        .center2 {
-            text-align: center;
-            font-family: Arial, Helvetica, sans-serif;
-            color: #ff8637;
-            font-size: 12px;
-            text-decoration: none;
-        }
-
-        .sub {
-            text-align: center;
-            color: #ff8637;
-            margin-top: -40px;
-            letter-spacing: 2px;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        a {
-            color: #ff8637;
-        }
-
-        a:hover {
-            color: #ee7727;
-        }
-    </style>
+    <meta name="description" content="Proxy or VPN service detected - FiveMods.net">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://assets.fivemods.net/">
+    <meta property="og:title" content="Proxy or VPN service detected - FiveMods.net">
+    <meta property="og:description" content="Proxy or VPN service detected - FiveMods.net">
+    <meta property="og:site_name" content="Proxy or VPN service detected - FiveMods.net">
+    <meta property="og:image" content="https://assets.fivemods.net/static-assets/img/svg/error/fivemods_error_503.svg">
+    <meta name="theme-color" content="#ff8637">
+    <meta name="msapplication-navbutton-color" content="#ff8637">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ff8637">
 </head>
-
 <body>
-    <h1 class="center" style="font-size: 75px;">Error</h1>
-    <p class="sub">You are not allowed to enter the website via VPN or proxy.</p>
-    <!-- <p class="center">Copyright &copy; 2020 FiveMods. All rights reserved.</p> -->
+    <main>
+        <section class="error-container">
+            <div class="error-fm-logo" ><a href="/"><img src="https://assets.fivemods.net/static-assets/img/svg/error/fivemods_error_proxy.svg" alt="FiveMods Proxy Error Logo"></a></div>
+            <h3>Proxy or VPN service detected</h3>
+            <p>You are not allowed to enter FiveMods.net via a VPN or proxy. Please refer to our system administrator if you think this is an error. Reach out via <a href="mailto:contact@fivemods.net">contact@fivemods.net</a>.</p>
+        </section>
+    </main>
 </body>
-
 </html>
