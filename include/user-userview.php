a18d1649ef7a51be76c13573251a076269e5ca8d
Fredneyy <69763370+Fredneyy@users.noreply.github.com>
2021-08-05 18:05:14 +0200

    lol forgot to echo it

--- include/user-userview.php
+++ include/user-userview.php
@@ -354,7 +354,7 @@ $articles = $articles->fetchAll(PDO::FETCH_ASSOC);
             }
             echo '
             </div>
-            <p>'; str_replace("&amp;", "&", $description); echo '</p></div>';
+            <p>'; echo str_replace("&amp;", "&", $description); echo '</p></div>';
             }
             ?>
       </div>
