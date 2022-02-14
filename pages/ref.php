cffa6b15d130b858e85cb880cc6ba9403e4a76ae
phhajek <hajek.ph@gmail.com>
2022-02-14 12:37:07 +0100

    Added ad trackingh

--- pages/ref.php
+++ pages/ref.php
@@ -6,118 +6,146 @@ ini_set('max_execution_time', 300);
 
 require_once('./config.php');
 
-if (isset($_GET['rdc'])) {
+// session_start();
 
-    $ref = urldecode($_GET['rdc']);
+if (isset($_GET['csrf']) && $_GET['add'] == "track") {
 
-    $suburl = getHost($ref);
-    $url = giveHost($suburl);
-
-    $stmt = $conn->prepare("SELECT marked_links FROM links WHERE marked_links LIKE ?");
-    $stmt->bind_param("s", $url);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            $link = $row['marked_links'];
-        }
-
-        $check = 1;
-
-        $output1 = '<section class="pt-5 pb-5">
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-        <div class="col-md-7">
-            <div class="card border border-danger">
-                <div class="card-body text-center p-5">
-                <h2 class="fas fa-exclamation-triangle text-danger"></h2>
-                <h3 class="pb-2 h3 mt-1 text-danger">'.$link.'</h3>
-                <p class="lead">This site is external and we <b>can\'t guarantee the safety</b>, always make sure to be on a website <b>using a SSL certificate</b>.</p>
-                <h5>This link grabs personal information, <b>we won\'t redirect you</b>.</h5>
-                </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    </section>';
-
-    include('./include/header-banner.php');
-    echo $output1;
-
+    if ($_GET['csrf'] != $_SESSION['csrfValidate']) {
+        unset($_SESSION['csrfValidate']);
+        header('location: /');
+        die("CSRF validation failed!");
+        exit();
 
     } else {
 
-        if (strpos($ref, 'https://') !==false) {
-            $link = $ref;
-        } else {
-            if (strpos($ref, 'http://') !==false) {
-                $strip = str_replace('http://', '', $ref);
-                $link = 'https://'.$strip;
-            } else {
-                $link = 'https://'.$ref;
-            }
+        $campaign = "London Studios";
+
+        $select = $pdo->query("SELECT * FROM ad_tracking WHERE campaign = '$campaign'")->fetchAll();
+        foreach ($select as $row) {
+            $click = $row['click'];
         }
+        $select = null;
+
+        $click = $click + 1;
 
 
-        $check = 0;
+        $insert = $pdo->prepare("UPDATE ad_tracking SET click=? WHERE campaign=?");
+        $insert->execute([$click, $campaign]);
+        $insert = null;
 
-        $output1 = '<section class="pt-5 pb-5">
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-        <div class="col-md-7">
-            <div class="card border border-success">
-                <div class="card-body text-center p-5">
-                <h3 class="pb-2 h3 mt-1 '.$css_text.'">'.$link.'</h3>
-                <p class="lead">This site is external and we <b>can\'t guarantee the safety</b>, always make sure to be on a website <b>using a SSL certificate</b>.</p>
-                <a href="'.$link.'" rel="nofollow noopener noreferrer" class="btn btn-xs btn-round btn-lg btn-success btn-rised mt-md-3">Continue and leave page</a> <br>
-                <a href="'.$link.'" class="btn btn-xs btn-round btn-sm btn-light btn-rised mt-md-3" rel="nofollow noopener noreferrer" target="_blank">Open link in new tab</a>
-                </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    </section>';
-
-    include('./include/header-banner.php');
-    echo $output1;
-
+        header('location: ' . $_GET['rel']);
     }
-
 } else {
 
-    $output1 = '<section class="pt-5 pb-5">
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-        <div class="col-md-7">
-            <div class="card border border-danger">
-                <div class="card-body text-center p-5">
-                <h2 class="fas fa-exclamation-triangle text-danger"></h2>
-                <h3 class="pb-2 h3 mt-1 text-danger">Error</h3>
-                <p class="lead">There was no link provided, to go back click the button.</p>
-                <a href="/" class="btn btn-xs btn-round btn-sm btn-light btn-rised mt-md-3">Go Back</a>
+    if (isset($_GET['rdc'])) {
+
+        $ref = urldecode($_GET['rdc']);
+
+        $suburl = getHost($ref);
+        $url = giveHost($suburl);
+
+        $stmt = $conn->prepare("SELECT marked_links FROM links WHERE marked_links LIKE ?");
+        $stmt->bind_param("s", $url);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                $link = $row['marked_links'];
+            }
+
+            $check = 1;
+
+            $output1 = '<section class="pt-5 pb-5">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+            <div class="col-md-7">
+                <div class="card border border-danger">
+                    <div class="card-body text-center p-5">
+                    <h2 class="fas fa-exclamation-triangle text-danger"></h2>
+                    <h3 class="pb-2 h3 mt-1 text-danger">' . $link . '</h3>
+                    <p class="lead">This site is external and we <b>can\'t guarantee the safety</b>, always make sure to be on a website <b>using a SSL certificate</b>.</p>
+                    <h5>This link grabs personal information, <b>we won\'t redirect you</b>.</h5>
+                    </div>
                 </div>
             </div>
+            </div>
         </div>
+        </section>';
+
+            include('./include/header-banner.php');
+            echo $output1;
+        } else {
+
+            if (strpos($ref, 'https://') !== false) {
+                $link = $ref;
+            } else {
+                if (strpos($ref, 'http://') !== false) {
+                    $strip = str_replace('http://', '', $ref);
+                    $link = 'https://' . $strip;
+                } else {
+                    $link = 'https://' . $ref;
+                }
+            }
+
+
+            $check = 0;
+
+            $output1 = '<section class="pt-5 pb-5">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+            <div class="col-md-7">
+                <div class="card border border-success">
+                    <div class="card-body text-center p-5">
+                    <h3 class="pb-2 h3 mt-1 ' . $css_text . '">' . $link . '</h3>
+                    <p class="lead">This site is external and we <b>can\'t guarantee the safety</b>, always make sure to be on a website <b>using a SSL certificate</b>.</p>
+                    <a href="' . $link . '" rel="nofollow noopener noreferrer" class="btn btn-xs btn-round btn-lg btn-success btn-rised mt-md-3">Continue and leave page</a> <br>
+                    <a href="' . $link . '" class="btn btn-xs btn-round btn-sm btn-light btn-rised mt-md-3" rel="nofollow noopener noreferrer" target="_blank">Open link in new tab</a>
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
-    </div>
-    </section>';
+        </section>';
 
-    include('./include/header-banner.php');
-    echo $output1;
+            include('./include/header-banner.php');
+            echo $output1;
+        }
+    } else {
 
+        $output1 = '<section class="pt-5 pb-5">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+            <div class="col-md-7">
+                <div class="card border border-danger">
+                    <div class="card-body text-center p-5">
+                    <h2 class="fas fa-exclamation-triangle text-danger"></h2>
+                    <h3 class="pb-2 h3 mt-1 text-danger">Error</h3>
+                    <p class="lead">There was no link provided, to go back click the button.</p>
+                    <a href="/" class="btn btn-xs btn-round btn-sm btn-light btn-rised mt-md-3">Go Back</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>';
 
-}
+        include('./include/header-banner.php');
+        echo $output1;
+    }
 
-function getHost($Address) {
-    $parseUrl = parse_url(trim($Address));
-    return trim($parseUrl['host'] ? $parseUrl['host'] : array_shift(explode('/', $parseUrl['path'], 2)));
-}
+    function getHost($Address)
+    {
+        $parseUrl = parse_url(trim($Address));
+        return trim($parseUrl['host'] ? $parseUrl['host'] : array_shift(explode('/', $parseUrl['path'], 2)));
+    }
 
-function giveHost($host_with_subdomain) {
-    $array = explode(".", $host_with_subdomain);
-    return (array_key_exists(count($array) - 2, $array) ? $array[count($array) - 2] : "").".".$array[count($array) - 1];
+    function giveHost($host_with_subdomain)
+    {
+        $array = explode(".", $host_with_subdomain);
+        return (array_key_exists(count($array) - 2, $array) ? $array[count($array) - 2] : "") . "." . $array[count($array) - 1];
+    }
 }
 
 
