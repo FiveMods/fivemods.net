2511993df8fdba15ef1ddcf8788c07769a78fdee
Philipp H <47315109+Phxl76@users.noreply.github.com>
2022-03-10 10:38:48 +0100

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -25,13 +25,9 @@
             <p>
                <a href="mailto://contact@fivemods.net">contact@fivemods.net</a>
             </p>
-            <p><strong><?php echo $lang['complaint'];?>?</strong></p>
-            <p>
-               <a href="mailto://complaint@fivemods.net?subject=General%20or%20legal%20complaint%20%7C%20SID%3A%20<?php echo session_id();?>">complaint@fivemods.net</a>
-            </p>
             <p><strong><?php echo $lang['privacy-manager'];?>:</strong></p>
             <p>
-               <a href="mailto:privacy@fivemods.net?subject=General%20privacy%20issue%20%7C%20SID%3A<?php echo session_id();?>">privacy@fivemods.net</a>
+               <a href="mailto:data-protection@fivemods.net?subject=General%20privacy%20issue%20%7C%20SID%3A<?php echo session_id();?>">data-protection@fivemods.net</a>
             </p>
          </div>
          <?php
